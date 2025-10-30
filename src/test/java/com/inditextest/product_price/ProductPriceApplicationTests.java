b6cb53c794b22d75250fea7265a8ec90b543b42e
Javier Partida Valle <javierpartidavalle@gmail.com>
2025-10-30 10:18:35 +0100

    initial commit

---
+++ src/test/java/com/inditextest/product_price/ProductPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditextest.product_price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
